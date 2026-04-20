6dc46d65e8adda9b3779a164acdf029c76c0d375
djacobs2@swarthmore.edu <djacobs2@swarthmore.edu>
2026-04-20 16:15:21 +0200

    started SIFT

--- panoramic_image_stiching.py
+++ panoramic_image_stiching.py
@@ -1,9 +1,14 @@
 import cv2
+import numpy as np
 
 img_paths = ["./Image-Data/harbor_1.jpg", "./Image-Data/harbor_2.jpg"]
 
 
 imgs = []
+features = []
+descriptors = []
+sift = cv2.SIFT_create()
+
 for i in range(0, len(img_paths)):
     img = cv2.imread(img_paths[i])
     if img is None:
@@ -11,7 +16,9 @@ for i in range(0, len(img_paths)):
         quit()
     imgs.append(img)
 
-### Step 1: Feature Detection using SIFT ###
+    ### Step 1: Feature Detection using SIFT ###
+    features[i], descriptors[i] = sift.detectAndCompute(img)
+    
 
 
 
