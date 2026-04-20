0f3051390eb5ba375714ae3e9335b8928057c5ff
djacobs2@swarthmore.edu <djacobs2@swarthmore.edu>
2026-04-20 16:54:50 +0200

    SIFT work

--- panoramic_image_stiching.py
+++ panoramic_image_stiching.py
@@ -7,19 +7,40 @@ img_paths = ["./Image-Data/harbor_1.jpg", "./Image-Data/harbor_2.jpg"]
 imgs = []
 features = []
 descriptors = []
+master_img = cv2.imread(img_paths[0])
 sift = cv2.SIFT_create()
 
-for i in range(0, len(img_paths)):
+
+
+### Step 1: Feature Detection using SIFT ###
+def match_images(img1, img2):
+    features1, descriptors1 = sift.detectAndCompute(img1, None)
+    features2, descriptors2 = sift.detectAndCompute(img2, None)
+
+    bfMatcher = cv2.BFMatcher(normType=cv2.NORM_L2)
+
+    # Lowe's ratio test:
+    # First, we match each feature in img1 with 2 (k=2) features in img2
+    # Then, we only use the feature if there is one match that is significantly better than the other--presumeably erroneous--matching
+    matches = bfMatcher.knnMatch(queryDescriptors=descriptors1, trainDescriptors=descriptors2, k=2)
+    best_matches = [m for m, n in matches if m.distance < 0.3*n.distance]
+
+    points1 = [features1[m.queryIdx] for m in best_matches]
+    matches = cv2.drawMatches(img1, features1, img2, features2, best_matches, None)
+
+    cv2.imshow("Feature matches", matches)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+for i in range(1, len(img_paths)):
     img = cv2.imread(img_paths[i])
     if img is None:
         print('Failed to load image')
         quit()
     imgs.append(img)
 
-    ### Step 1: Feature Detection using SIFT ###
-    features[i], descriptors[i] = sift.detectAndCompute(img)
-    
-
+    match_images(master_img, img)
 
 
 
